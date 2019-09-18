f9a286d0a4b56cace59c1923217b8798368ed7a9
Alexis Carranza <aleexiz1996@gmail.com>
2019-09-17 21:16:17 -0300

    Se crea un nuevo proyecto Express, y se utiliza mongoDB

--- app.js
+++ app.js
@@ -1,44 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json({}));
-app.use(bodyParser.urlencoded({extended: false}));
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+//Importar datos
+const db = require('./config/database.js');
+db.connect();
+const Place = require('./models/Place');
+//
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const places = [
-  {
-    'title': 'Oficina Codigo',
-    'description': 'Lorem Ipsum',
-    'addres': 'Lorem IPsum'
-  },
-  {
-    'title': 'Oficina Codigo',
-    'description': 'Lorem Ipsum',
-    'addres': 'Lorem IPsum'
-  },
-  {
-    'title': 'Oficina Codigo',
-    'description': 'Lorem Ipsum',
-    'addres': 'Lorem IPsum'
-  },
-  {
-    'title': 'Oficina Codigo',
-    'description': 'Lorem Ipsum',
-    'addres': 'Lorem IPsum'
-  }
-];
+var app = express();
 
-app.get('/', (req,res)=>{
-  res.json(places);res.send('Hola mundo');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//Creando un nuevo lugar
+app.post('/places', (req,res)=>{
+  Place.create({
+    title: req.body.title,
+    description: req.body.description,
+    accepsCreditCard: req.body.accepsCreditCard,
+    openHour: req.body.openHour,
+    closeHour: req.body.closeHour
+    }).then(doc => {
+      res.json(doc);
+    }).catch(err=>{
+      console.log(err);
+      res.json(err);
+    });
 });
 
-app.post('/', (req,res)=>{
-  res.json(req.body.nombres);
-})
-
-app.use(express.static('public'));
-
-
-
-app.listen(3000,function(){
-  console.log('Estoy listo para recibir peticiones')
+app.get('/places', (req,res)=>{
+  Place.find({}).then(docs=>{
+    res.json(docs);
+  }).catch(err => {
+    console.log(err);
+    res.json(err);
+  })
 });
+
+app.get('/places/:id', (req,res)=>{
+  Place.findById(req.params.id)
+    .then(doc=>{
+      res.json(doc);
+    }).catch(err=>{
+      console.log(err);
+      res.json(err);
+    })
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+module.exports = app;
