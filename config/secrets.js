73c5712769350af3d55a9c49c183c3a818d0f0e3
Alexis Fabian Carranza <alexis.carranza.sistemas@gmail.com>
2019-09-28 17:43:38 -0300

    Se elimina secret por seguridad

--- config/secrets.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    cloudinary: {
-        api_key: '461589499659481',
-        cloud_name: 'dghncnivo',
-        api_secret: '4B7MZnqswA05_UBZmHdGkV2JeW8',
-    },
-    jwtSecret: 'aljsdvbfosabpfiabspidufbpisaudbfpiasubdfpiuabsipdf'
-}
